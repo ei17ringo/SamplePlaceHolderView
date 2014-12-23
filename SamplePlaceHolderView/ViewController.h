df0c7a1e2ba0e2934007005c543edeb8f20c743e
ei17ringo <ei17ringo@gmail.com>
2014-12-23 13:44:45 +0800

    Initial Commit

---
+++ SamplePlaceHolderView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SamplePlaceHolderView
+//
+//  Created by Eriko Ichinohe on 2014/12/23.
+//  Copyright (c) 2014年 Eriko Ichinohe. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
